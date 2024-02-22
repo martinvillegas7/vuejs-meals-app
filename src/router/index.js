e4baf2a3dfee9927d325a0edf1e8d10b1717c58d
mvillegas2 <martin.villegas@ibox.com.co>
2024-02-21 19:58:17 -0500

    fixing some components

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import DefaultLayout from '../components/DefaultLayout.vue'
+import GuestLayout from '../components/GuestLayout.vue'
+import Home from '../views/Home.vue'
+import MealsByName from '../views/MealsByName.vue'
+import MealsByLetter from '../views/MealsByLetter.vue'
+import MealsByIngredient from '../views/MealsByIngredient.vue'
+import MealDetails from '../views/MealDetails.vue'
+import Ingredients from '../views/Ingredients.vue'
 
 
 const routes = [
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,73 +1,23 @@
 <template>
-    <div class="flex flex-col-2 items-center content-center">
-        <!-- <div>
-            <h1>Alégrate con la única
-                Contabilidad inteligente
-                Facturación electrónica
-                Venta POS
-                Nómina electrónica
-                que le da superpoderes a tu pyme</h1>
-            <p>Vive el poder de vender, facturar y contabilizar de manera automática y desde un solo lugar.</p>
-            <YoutubeButton>Hola</YoutubeButton>
-            <p>
-                Valoración de 5
-
-                Hubspot LogoCapterra LogoPlayStore Logo</p>
-        </div> -->
-        <img class="flex translate-y-5" src="../assets/alegra-COL-Home-hero.webp" alt="">
-
+    <div class="p-8 pb-0 text-orange-500">
+        <h1 class="text-4xl font-bold mb-4">Random Meals</h1>
     </div>
-    <div class="flex flex-col-2 items-center content-center">
-        <!-- <div>
-            <h1>Alégrate con la única
-                Contabilidad inteligente
-                Facturación electrónica
-                Venta POS
-                Nómina electrónica
-                que le da superpoderes a tu pyme</h1>
-            <p>Vive el poder de vender, facturar y contabilizar de manera automática y desde un solo lugar.</p>
-            <YoutubeButton>Hola</YoutubeButton>
-            <p>
-                Valoración de 5
-
-                Hubspot LogoCapterra LogoPlayStore Logo</p>
-        </div> -->
-        <img class="flex translate-y-5" src="../assets/alegra-COL-Home-hero.webp" alt="">
-
-    </div>
-    <div class="flex flex-col-2 items-center content-center">
-        <!-- <div>
-            <h1>Alégrate con la única
-                Contabilidad inteligente
-                Facturación electrónica
-                Venta POS
-                Nómina electrónica
-                que le da superpoderes a tu pyme</h1>
-            <p>Vive el poder de vender, facturar y contabilizar de manera automática y desde un solo lugar.</p>
-            <YoutubeButton>Hola</YoutubeButton>
-            <p>
-                Valoración de 5
-
-                Hubspot LogoCapterra LogoPlayStore Logo</p>
-        </div> -->
-        <img class="flex translate-y-5" src="../assets/alegra-COL-Home-hero.webp" alt="">
-
-    </div>
+    <Meals :meals="meals" />
 </template>
-
+  
 <script setup>
-import { computed, onMounted, ref } from 'vue';
-import store from '../store';
-import axiosClient from '../axiosClient.js';
+import { computed, onMounted, ref } from "vue";
+import store from "../store";
+import Meals from "../components/Meals.vue";
+import axiosClient from "../axiosClient.js";
 
-const meals = computed(() => store.state.meals)
-const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
-const ingredients = ref([]);
+const meals = ref([]);
 
 onMounted(async () => {
-    const response = await axiosClient.get('/list.php?i=list')
-    console.log(response.data)
-    ingredients.value = response.data
-})
+    for (let i = 0; i < 10; i++) {
+        axiosClient
+            .get(`random.php`)
+            .then(({ data }) => meals.value.push(data.meals[0]));
+    }
+});
 </script>
-
